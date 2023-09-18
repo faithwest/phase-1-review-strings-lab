6307eb61855cba25ddea06d3b489e760a49c4c05
faithwest <faithchepkoech425@gmail.com>
2023-09-18 15:25:48 +0300

    string test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, currentUser' + 'Grace Hopper!';
+const excitedWelcomeMessage = "Welcome to Flatbook, + Grace Hopper!".toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
